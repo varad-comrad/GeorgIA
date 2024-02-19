5aae0c0179740f4d4915d9f489ffea7a93ed975a
varad-comrad <fabricio_asfora_002@hotmail.com>
2024-02-19 18:11:33 -0300

    created dataset generator

---
+++ dataset_gen.py
@@ -0,0 +1,24 @@
+import subprocess
+import pathlib
+
+
+commands = [('.', 'mkdir datasets'),
+            ('datasets', 'kaggle datasets download -d annaglass1/geoguessr-55countries'),
+            ('datasets', 'kaggle datasets download -d ubitquitin/geolocation-geoguessr-images-50k')]
+
+
+def check_for_datasets():
+    base_path = pathlib.Path(__file__).parent / 'datasets'
+    datasets = base_path / 'geolocation-geoguessr-images-50k.zip', base_path / ''
+    if not (base_path.exists() and all(dataset.exist() for dataset in datasets)):
+        create_dataset()
+
+
+def check_kaggle_api():
+    ret = subprocess.run('kaggle -h', shell=True, capture_output=True)
+    return bool(ret.stderr)
+
+def create_dataset():
+    for cwd, command in commands:
+        subprocess.run(command, shell=True, cwd=cwd)
+    print('Dataset created')
